59913b661fc86cc93999f65122c2c524dc7dfd10
AryanShinde <you@example.com>
2021-03-20 19:49:14 +0530

    added firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmexUHNv0_HQo1a0FTCT1VKO6QmwSNpms",
+    authDomain: "docstory-22a83.firebaseapp.com",
+    databaseURL: "https://docstory-22a83-default-rtdb.firebaseio.com",
+    projectId: "docstory-22a83",
+    storageBucket: "docstory-22a83.appspot.com",
+    messagingSenderId: "554766187672",
+    appId: "1:554766187672:web:e2abdfff67a813bc9ce4ed",
+    measurementId: "G-625FBQVW3K"
+  };
+  
