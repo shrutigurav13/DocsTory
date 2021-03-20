36274e71c8f72509e508ef2a0c203687b7944292
AryanShinde <you@example.com>
2021-03-20 19:49:29 +0530

    added firebase

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 import React, { useState } from "react";
 import "./styles.css";
-export default function App() {
-    var [userinput, setuserinput] = useState("");
-    var [meaning, setmeaning] = useState("");}
